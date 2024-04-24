d581b9879453c73db8724eea9e1a1aa2ba103e24
Joel <joelluis73@gmail.com>
2024-04-23 23:01:18 -0300

    chore: add husly and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+const name = "";
+
+console.log(name);
+
 export default function Home() {
-  return <div className="bg-red-500 px-4">Hello tailwind</div>
+  return <div className="bg-red-500 px-4">Hello tailwind</div>;
 }
